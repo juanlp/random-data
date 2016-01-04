1fdfbe0ff1971439180d800822f2886490a42186
juanlp <juan.luong@gmail.com>
2016-01-04 20:50:11 +0800

    data collection

---
+++ testrob.R
@@ -0,0 +1,30 @@
+library(RSelenium)
+library(rvest)
+
+
+
+checkForServer()
+startServer()
+remDr <- remoteDriver(browserName = "phantomjs")
+remDrv$open()
+
+
+remDrv$navigate("http://www.footywire.com/afl/footy/ft_match_statistics?mid=6171")
+
+test.html <- htmlParse(remDrv$getPageSource()[[1]]) #html is deprecated in new version of rvest
+
+
+table <- remDrv$getPageSource()[[1]] %>% 
+  htmlParse %>% 
+  readHTMLTable
+
+table.sel = NULL
+for (i in 1:length(table)){
+  if(table[i][[1]][1,1] == "Player") {table.sel = c(table.sel,i)}
+}
+
+table.sel
+
+table[[table.sel[1]]]
+table[[table.sel[2]]]
+
